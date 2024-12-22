d0b3e175dc2ea24e8422bb605665d7665e23a78e
csh3424 <csh3424@icloud.com>
2024-12-22 23:55:11 +0900

    세부 페이지 추가 및 js 파일 추가

---
+++ script.js
@@ -0,0 +1,12 @@
+// Add event listeners to each timeline box
+document.getElementById("past").addEventListener("click", function() {
+  window.location.href = "past.html";
+});
+
+document.getElementById("present").addEventListener("click", function() {
+  window.location.href = "present.html";
+});
+
+document.getElementById("future").addEventListener("click", function() {
+  window.location.href = "future.html";
+});
